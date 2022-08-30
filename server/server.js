1484c71b5534bea257e678825857203a052381f2
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2022-08-30 21:34:22 +0700

    publish link routes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const config = require("./app/config");
+const app = express();
 
+
+const setUpProductRoutes = require("./app/routes/product.router");
+const setUpBrandRoutes = require("./app/routes/brand.router");
+
+app.use(cors());
+app.use(express.json());
+
+
+setUpProductRoutes(app);
+setUpBrandRoutes(app);
+
+app.get("/", (req, res) => { 
+    res.send({message:"hello world"});
+});
+
+const PORT = config.app.port;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}.`);
+});
